bbd872ac1ea00417d1d33e30e5b07520de5c348d
PavelGem <pashentsev.1@gmail.com>
2020-07-15 18:07:22 +0300

    create alpha example scene

---
+++ Creditor/Assets/Pavel example_model/Scripts/GameManager_example.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_example : MonoBehaviour
+{
+    public GameObject label;
+/*    public GameObject computer;*/
+    public GameObject store;
+    public GameObject[] environment;
+    public GameObject startButton;
+    private void Start()
+    {
+        label.SetActive(true);
+/*        computer.SetActive(true);*/
+        //store.SetActive(true); его пока нет
+        foreach (GameObject smth in environment)
+        {
+            smth.SetActive(false);
+        }
+        startButton.SetActive(true);
+    }
+    public void StartTheGame()
+    {
+        label.SetActive(false);
+        startButton.SetActive(false);
+        /*        computer.SetActive(true);*/
+        //store.SetActive(true); его пока нет
+        foreach (GameObject smth in environment)
+        {
+            smth.SetActive(true);
+        }
+    }
+}
