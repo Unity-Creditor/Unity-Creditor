c2ff68ff9ff360f51701ab29e4b03b1006913cbe
PavelGem <pashentsev.1@gmail.com>
2020-07-16 12:15:14 +0300

    create main scene

---
+++ Creditor/Assets/Simon swipes/Scripts/SwipeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class SwipeScript : MonoBehaviour
+{
+   public UnityEvent OnSwipe = new UnityEvent();
+   bool MouseDown = false;
+
+   public void OnMouseDown()
+   {
+     MouseDown = true;
+   }
+
+   public void OnMouseUp()
+   {
+     MouseDown = false;
+     if (Mathf.Abs(transform.position.x) >= 1.25f)
+     {
+       Debug.Log("I am here");
+       OnSwipe.Invoke();
+       Destroy(transform.GetChild(0).gameObject);
+       Destroy(gameObject);
+     }
+     else
+     {
+       transform.position = new Vector3(0, transform.position.y, transform.position.z);
+     }
+
+   }
+
+   public void Update()
+   {
+     if (MouseDown)
+     {
+       Vector3 actualPosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y, 0);
+       transform.position = actualPosition;
+     }
+   }
+}
