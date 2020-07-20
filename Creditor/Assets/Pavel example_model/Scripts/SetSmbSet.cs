0ba1f5103857834958dea43130660497fd804dc6
PavelGem <pashentsev.1@gmail.com>
2020-07-21 00:17:17 +0300

    change custom

---
+++ Creditor/Assets/Pavel example_model/Scripts/SetSmbSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSmbSet : MonoBehaviour
+{
+    public string name;
+    public void ChoseSet(int value)
+    {
+        PlayerPrefs.SetInt(name, value);
+    }
+}
