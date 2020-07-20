9224fbe53838c9f238433ebf87dd889dfdbe90d9
PavelGem <pashentsev.1@gmail.com>
2020-07-20 13:25:14 +0300

    main menu counters

---
+++ Creditor/Assets/Pavel example_model/Scripts/CounterSCR.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CounterSCR : MonoBehaviour
+{
+    public string nameOfTheDlta;
+    
+    void Awake()
+    {
+        PlayerPrefs.SetInt(name,0);
+        GetComponentInChildren<TextMeshProUGUI>().text = PlayerPrefs.GetInt(nameOfTheDlta).ToString();
+    }
+
+}
