be931427c882c19dc16e1da67624b7244728d40c
Sversim <Sversim@gmail.com>
2020-07-21 18:09:23 +0600

    Custom scene v(n-1)
    Примерно построенная сцена

--- Creditor/Assets/Scripts/CustomSceneControllerScript.cs
+++ Creditor/Assets/Scripts/CustomSceneControllerScript.cs
@@ -6,13 +6,17 @@ public class CustomSceneControllerScript : MonoBehaviour
 {
     public ArraysLayout ScenesItems;
     public int[] ItemNumber = new int [13];
-    //public  int[,] ScenesItems = new int[5, 13];
     //public  GameObject[,] faces, bodies, eyes, mouth, hairstyles, ties, computers, cup, books, tables, armchair, background;
 
 
     void Start()
     {
-      //for(int i = 0; )
+      //Debug.Log(ScenesItems.rows[0].row[0]);
+      for(int i = 0; i < ScenesItems.rows.Length; ++i)
+      {
+        Instantiate(ScenesItems.rows[i].row[ItemNumber[i]]);
+
+      }
     }
 
     // Update is called once per frame
