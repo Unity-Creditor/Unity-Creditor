351c918743bd4872fd637948de3490fb46e7776f
Sversim <Sversim@gmail.com>
2020-07-21 17:01:42 +0600

    Bening

---
+++ Creditor/Assets/Scripts/CustomSceneControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSceneControllerScript : MonoBehaviour
+{
+    public ArraysLayout ScenesItems;
+    public int[] ItemNumber = new int [13];
+    //public  int[,] ScenesItems = new int[5, 13];
+    //public  GameObject[,] faces, bodies, eyes, mouth, hairstyles, ties, computers, cup, books, tables, armchair, background;
+
+
+    void Start()
+    {
+      //for(int i = 0; )
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Creditor/Assets/Scripts/LaptopScript.cs
+++ Creditor/Assets/Scripts/LaptopScript.cs
@@ -7,55 +7,22 @@ public class LaptopScript : Manager<LaptopScript>
 
     void Start()
     {
-        /*        for (int i = 0; i != ScreenArts.Length; ++i)
-                {
-                    ScreenArts[i].SetActive(false);
-                }
+        for (int i = 0; i != ScreenArts.Length; ++i)
+        {
+          ScreenArts[i].SetActive(false);
+        }
 
-                ScreenArts[0].SetActive(true);*/
-        ScreenArts[0].SetActive(false);
-        ScreenArts[1].SetActive(false);
-        ScreenArts[2].SetActive(false);
-
+        ScreenArts[0].SetActive(true);
     }
 
     public void ChangeGraph(int GraphControl)
     {
+      if (GraphControl != ActualGraph)
+      {
+        ScreenArts[ActualGraph].SetActive(false);
         ActualGraph = GraphControl;
-        /*Debug.Log("Graph changed");*/
-        if (GraphControl == 0)
-        {
-            ScreenArts[0].SetActive(true);
-            ScreenArts[1].SetActive(false);
-            ScreenArts[2].SetActive(false);
-            Debug.Log("Graph changed 0");
-        }
-        if (GraphControl == 1)
-        {
-            ScreenArts[0].SetActive(false);
-            ScreenArts[1].SetActive(true);
-            ScreenArts[2].SetActive(false);
-            Debug.Log("Graph changed 1");
-        }
-        if (GraphControl == 2)
-        {
-            ScreenArts[0].SetActive(false);
-            ScreenArts[1].SetActive(false);
-            ScreenArts[2].SetActive(true);
-            Debug.Log("Graph changed 2");
-        }
-/*        if (GraphControl == 3)
-        {
-            ScreenArts[0].SetActive(true);
-            ScreenArts[1].SetActive(false);
-            ScreenArts[2].SetActive(false);
-        }*/
-        /*      if (GraphControl != ActualGraph)
-              {
-                ScreenArts[ActualGraph].SetActive(false);
-                ActualGraph = GraphControl;
-                ScreenArts[GraphControl].SetActive(true);
-              }*/
+        ScreenArts[GraphControl].SetActive(true);
+      }
 
     }
 }
