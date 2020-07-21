945664236a80e1f9e5a6afd1e080db6df894628f
PavelGem <pashentsev.1@gmail.com>
2020-07-21 17:51:02 +0300

    update

--- Creditor/Assets/Pavel example_model/Scripts/Loader.cs
+++ Creditor/Assets/Pavel example_model/Scripts/Loader.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Loader : MonoBehaviour
 {
     public int loadScene;
+    public int level;
+    public int maxScore = 2;
+    private void Start()
+    {
+        PlayerPrefs.SetInt("openedLevel", 1);
+        if (PlayerPrefs.GetInt("openedLevel") >= level)
+        {
+            GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
 
     public void LoadMyScene()
     {
+        SceneManager.LoadScene(loadScene);
+    }
+
+    public void LoadLevel()
+    {
+        PlayerPrefs.SetInt("maxScore",20);
         SceneManager.LoadScene(loadScene);
     }
 }
