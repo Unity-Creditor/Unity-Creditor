caf7f0809542573772df81f3376d07889def657d
Sversim <Sversim@gmail.com>
2020-07-22 14:45:03 +0600

    Shop v(n)

--- Creditor/Assets/Scripts/CustomSceneControllerScript.cs
+++ Creditor/Assets/Scripts/CustomSceneControllerScript.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CustomSceneControllerScript : MonoBehaviour
+public class CustomSceneControllerScript : Manager<CustomSceneControllerScript>
 {
     public ArraysLayout ScenesItems;
+    public Button[] StoreButtons = new Button[1];
+    public GameObject[] ActiveObjects;
+    public Vector3 StartButtonPosition = new Vector3(0, -710, 0);
+    public int ActivatedShop;
     public int[] ItemNumber = new int [13];
-    //public  GameObject[,] faces, bodies, eyes, mouth, hairstyles, ties, computers, cup, books, tables, armchair, background;
-
 
     void Start()
     {
-      //Debug.Log(ScenesItems.rows[0].row[0]);
+
+      ActiveObjects = new GameObject[ScenesItems.rows.Length];
+
       for(int i = 0; i < ScenesItems.rows.Length; ++i)
       {
-        Instantiate(ScenesItems.rows[i].row[ItemNumber[i]]);
-
+        ActiveObjects[i] = Instantiate(ScenesItems.rows[i].row[ItemNumber[i]]);
       }
+
+      /*for (int i = 0; i < StoreButtons.Length; i++)
+      {
+        StoreButtons[i].onClick.AddListener( () => ChangeStore(i-1) );
+      }*/
+      //Debug.Log(ScenesItems.rows[0].row[0]);
     }
 
-    // Update is called once per frame
+    public void ChangeStore(int num = 0)
+    {
+
+      Debug.Log(num + "" + ActivatedShop);
+      StoreButtons[ActivatedShop].GetComponent<Image>().color = Color.white;
+      StoreButtons[num].GetComponent<Image>().color = Color.cyan;
+
+      ActivatedShop = num;
+
+    }
+
     void Update()
     {
 
