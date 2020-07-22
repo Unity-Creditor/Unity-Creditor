31998753a972ae468adb23cffcf0050eefafd07d
Sversim <Sversim@gmail.com>
2020-07-22 16:32:28 +0600

    Custom v(n+1)

--- Creditor/Assets/Scripts/CustomSceneControllerScript.cs
+++ Creditor/Assets/Scripts/CustomSceneControllerScript.cs
@@ -7,41 +7,56 @@ public class CustomSceneControllerScript : Manager<CustomSceneControllerScript>
 {
     public ArraysLayout ScenesItems;
     public Button[] StoreButtons = new Button[1];
+    public Button[] StoreItemsButton = new Button[5];
     public GameObject[] ActiveObjects;
     public Vector3 StartButtonPosition = new Vector3(0, -710, 0);
+
     public int ActivatedShop;
+    int PreviousActivatedShop = -1;
+
     public int[] ItemNumber = new int [13];
+
 
     void Start()
     {
 
       ActiveObjects = new GameObject[ScenesItems.rows.Length];
-
       for(int i = 0; i < ScenesItems.rows.Length; ++i)
       {
         ActiveObjects[i] = Instantiate(ScenesItems.rows[i].row[ItemNumber[i]]);
       }
-
-      /*for (int i = 0; i < StoreButtons.Length; i++)
-      {
-        StoreButtons[i].onClick.AddListener( () => ChangeStore(i-1) );
-      }*/
       //Debug.Log(ScenesItems.rows[0].row[0]);
     }
+
 
     public void ChangeStore(int num = 0)
     {
 
       Debug.Log(num + "" + ActivatedShop);
       StoreButtons[ActivatedShop].GetComponent<Image>().color = Color.white;
-      StoreButtons[num].GetComponent<Image>().color = Color.cyan;
+      StoreButtons[num].GetComponent<Image>().color = Color.gray;
 
       ActivatedShop = num;
 
     }
 
-    void Update()
-    {
 
+    public void ChangeActiveObject(int num = 0)
+    {
+      Destroy(ActiveObjects[ActivatedShop]);
+      ActiveObjects[ActivatedShop] = Instantiate(ScenesItems.rows[ActivatedShop].row[num]);
+      ItemNumber[ActivatedShop] = num;
     }
+
+
+    /*void Update()
+    {
+      if (PreviousActivatedShop != ActivatedShop)
+      {
+        for (int i = 0; i < StoreItemsButton.Length; i++)
+        {
+
+        }
+      }
+    }*/
 }
